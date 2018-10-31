b56b2476e3bb5263e02d939b2ac7706b546a4b45
prabuarjunan <31085459+prabuarjunan@users.noreply.github.com>
2018-10-30 22:22:53 -0700

    Update getRequest.py

--- getRequest.py
+++ getRequest.py
@@ -4,8 +4,7 @@ import time
 
 #Base URL
 CVAPI_BASEURL="https://cds-aws-bundles.netapp.com:8080/v1"
-CVAPI_APIKEY = "b3ZNd2lxN0VaV0U1akNaRW9JZlFKbUJkM0xNWEwz"
-CVAPI_SECRETKEY = "SThKYXFFeVVsSVROU3BhNjhjekdQY0VteHl5ZG1h"
+
 
 
 #Headers
